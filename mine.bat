9fb7759e36691097339f51fea6a8b74df164fc13
xD5556 <ionutenescu98@gmail.com>
2021-05-05 09:17:52 +0300

    update command

--- mine.bat
+++ mine.bat
@@ -1,2 +1,1 @@
-miner.exe -algo grin32 -server stratum+tcp://grin32-eu.f2pool.com:13654 -user grin1fru7rwntc5p0eg3q35tvnuz4z3npfscyst0pflrpf72jer5d22jq73pxpa.worker1
-pause
+miner.exe --algo grin32 --server stratum+tcp://grin32-eu.f2pool.com:13654 --user grin1fru7rwntc5p0eg3q35tvnuz4z3npfscyst0pflrpf72jer5d22jq73pxpa.worker1

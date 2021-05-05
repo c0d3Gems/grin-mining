7e0ff3ee8ffc02349c428a30925c3622e8e180f0
xD5556 <ionutenescu98@gmail.com>
2021-05-05 09:11:27 +0300

    remove unneccessary files

--- mine_bittube.bat
+++
@@ -1,2 +0,0 @@
-miner.exe --algo cuckaroo29b --server bittube.herominers.com:10281 --user Tubsa51t6TUammkT6U6DGfDXTocBt2okEAq9RwK7MFbK66X5g9VkGEFSdpwXoa5Ez36Z2aX5ooD6RTysiyii95jbB5iX8MPNdjo
-pause
--- mine_ycash.bat
+++
@@ -1,2 +0,0 @@
-miner.exe --algo 192_7 --pers ZcashPoW --server yec.f2pool.com:4500 --user s1ZpDP65RjYqNpRRnP5DB9fSsUJEpQKJDNj --pass x
-pause
